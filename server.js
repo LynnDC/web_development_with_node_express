cbe772b0a284e04086bf237c352a65e208237bda
Lynn Chen <lynn.d.chen@gmail.com>
2017-09-09 17:19:29 -0700

    'adding route to server'

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.static('./public'));
 
+app.get(' /', (req, res) => {res.type('text/plain'); res.send('Meadowlark Travel');});
+app.get('/about', (req,res) =>{res.type('text/plain'); res.send('About Meadowlark Travel');})
+
 // custom 404 page
 app.use((req,res) => {res.type('text/plain'); res.status(404); res.send('404- Not Found');});
 
